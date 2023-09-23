dab34521494d4ceaa118811c8374f80560f0ed14
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-22 22:51:04 -0500

    comment out use of 'window'

--- packages/client/src/ui/logo.tsx
+++ packages/client/src/ui/logo.tsx
@@ -8,19 +8,19 @@ interface ILogoProps {
   figureClassName: string;
 }
 export const Logo: React.FC<ILogoProps> = ({ figureClassName }) => {
-  const { theme } = useTheme();
-  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'light';
+  // const { theme } = useTheme();
+  // const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+  //   ? 'dark'
+  //   : 'light';
 
-  const src =
-    theme === 'system'
-      ? systemTheme === 'dark'
-        ? '/logo-white.svg'
-        : '/logo-color.svg'
-      : theme === 'dark'
-      ? '/logo-white.svg'
-      : '/logo-color.svg';
+  // const src =
+  //   theme === 'system'
+  //     ? systemTheme === 'dark'
+  //       ? '/logo-white.svg'
+  //       : '/logo-color.svg'
+  //     : theme === 'dark'
+  //     ? '/logo-white.svg'
+  //     : '/logo-color.svg';
 
   return (
     <figure className={cn('mx-auto', figureClassName)}>
