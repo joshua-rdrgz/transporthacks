6efd7ba0eadc9b2e6ca5f75963959af78d719010
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-23 11:34:38 -0500

    Create MatchesClient

---
+++ packages/client/src/app/matches/MatchesClient.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { Match } from '@/features/matches/Match';
+import { TCurrentUser } from '@/services/apiAuth';
+import axios from 'axios';
+
+interface IMatchesClientProps {
+  currentUser: TCurrentUser;
+}
+
+const DUMMY_POTENTIAL_MATCHES = [
+  {
+    name: 'Jane Smith',
+    email: 'janesmith@fakeemail.com',
+    startPoint: '123 Main St., Anywhere TX 12345',
+    endPoint: '456 Second St., Anywhere TX 12345',
+  },
+  {
+    name: 'Joseph Pratt',
+    email: 'josephpratt@fakeemail.com',
+    startPoint: '123 5th St., Anywhere TX 12345',
+    endPoint: '456 Garland Ave., Anywhere TX 12345',
+  },
+  {
+    name: 'Mark Zuckerburg',
+    email: 'markyboi@meta.com',
+    startPoint: '58423 9th St., Anywhere TX 12345',
+    endPoint: '456 Austin Blvd., Anywhere TX 12345',
+  },
+];
+
+const DUMMY_OFFICIAL_MATCHES = [
+  {
+    name: 'Jeff Bezos',
+    email: 'jeffyboi@amazon.com',
+    startPoint: '9825 8th St., Anywhere TX 12345',
+    endPoint: '4321 Transport Ave., Anywhere TX 12345',
+  },
+];
+
+export const MatchesClient: React.FC<IMatchesClientProps> = ({
+  currentUser,
+}) => {
+  const onClick = async () => {
+    const result = await axios.post('/api/maintenance', {
+      test: 'this is from the client.',
+    });
+    console.log(result);
+  };
+
+  return (
+    <>
+      {/* Find a {BLANK = 'Driver' | 'Commuter'} */}
+      <h1 className='text-5xl font-bold text-center'>Find a Partner</h1>
+      <div className='flex flex-col gap-8'>
+        <section>
+          <h2 className='text-2xl font-semibold mb-3'>
+            Your Potential Matches
+          </h2>
+          <div className='flex flex-col gap-5'>
+            {DUMMY_POTENTIAL_MATCHES.map((match) => {
+              return <Match key={match.name} match={match} />;
+            })}
+          </div>
+        </section>
+        <section>
+          <h2 className='text-2xl font-semibold mb-3'>Your Official Matches</h2>
+          <div className='flex flex-col gap-5'>
+            {DUMMY_OFFICIAL_MATCHES.map((match) => {
+              return <Match key={match.name} match={match} />;
+            })}
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
