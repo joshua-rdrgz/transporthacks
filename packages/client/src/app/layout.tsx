d301df31de0f732d0b2e3fd37c3ef699a0d5404e
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-22 23:54:39 -0500

    wrap children in main

--- packages/client/src/app/layout.tsx
+++ packages/client/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider attribute='class' defaultTheme='system'>
           <ToasterProvider />
-          {children}
+          <main>{children}</main>
         </ThemeProvider>
       </body>
     </html>
