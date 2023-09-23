44e238929cb2408c2e8d5f32d39772e8322eea68
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-22 22:53:13 -0500

    update register route, remove passwordHash from return

--- packages/client/src/app/api/register/route.ts
+++ packages/client/src/app/api/register/route.ts
@@ -1,24 +1,24 @@
 import bcrypt from 'bcrypt';
-import prisma from '@/libs/prisma';
+import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 const SALT_LENGTH = 12;
 export async function POST(request: Request) {
-  const { email, name, password, status, startPoint, endPoint } =
-    await request.json();
+  const { email, name, password } = await request.json();
 
   const passwordHash = await bcrypt.hash(password, SALT_LENGTH);
 
   const user = await prisma.user.create({
     data: {
-      email,
       name,
+      email,
       passwordHash,
-      status,
-      startPoint,
-      endPoint,
     },
   });
 
-  return NextResponse.json(user);
+  return NextResponse.json({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  });
 }
