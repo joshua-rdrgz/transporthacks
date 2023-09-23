a99c9825d125662b09b22febb48cdaef1955860d
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-22 22:50:51 -0500

    Include Dark Mode Toggle

--- packages/client/src/ui/auth-page-layout.tsx
+++ packages/client/src/ui/auth-page-layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { ModeToggle } from '@/features/theme/ModeToggle';
 import { Logo } from '@/ui/logo';
 import { Button } from '@/ui/button';
 
@@ -22,27 +23,32 @@ export const AuthPageLayout: React.FC<IAuthPageLayoutProps> = ({
   footerLinksTo,
 }) => {
   return (
-    <div className='flex flex-col justify-between min-h-screen py-10 px-7 text-center bg-secondary'>
-      <header className='text-4xl'>
-        <Logo figureClassName='w-1/3' />
-      </header>
-      <main>
-        <div className='flex flex-col gap-4'>
-          <h1 className='font-display text-5xl text-primary'>{formTitle}</h1>
-          <p className='text-primary'>{formSubtitle}</p>
-          <div className='w-full mx-auto bg-background rounded-md py-5 px-6 sm:w-3/4 md:w-1/2 xl:w-1/3'>
-            {formComponent}
+    <>
+      <div className='text-right p-3'>
+        <ModeToggle />
+      </div>
+      <div className='flex flex-col justify-between min-h-screen py-10 px-7 text-center bg-secondary'>
+        <header className='text-4xl'>
+          <Logo figureClassName='w-1/3' />
+        </header>
+        <main>
+          <div className='flex flex-col gap-4'>
+            <h1 className='font-display text-5xl text-primary'>{formTitle}</h1>
+            <p className='text-primary'>{formSubtitle}</p>
+            <div className='w-full mx-auto bg-background rounded-md py-5 px-6 sm:w-3/4 md:w-1/2 xl:w-1/3'>
+              {formComponent}
+            </div>
           </div>
-        </div>
-      </main>
-      <footer>
-        {footerLabel}{' '}
-        <Link href={footerLinksTo}>
-          <Button variant='link' className='text-md'>
-            {footerLinkLabel}
-          </Button>
-        </Link>
-      </footer>
-    </div>
+        </main>
+        <footer>
+          {footerLabel}{' '}
+          <Link href={footerLinksTo}>
+            <Button variant='link' className='text-md'>
+              {footerLinkLabel}
+            </Button>
+          </Link>
+        </footer>
+      </div>
+    </>
   );
 };
