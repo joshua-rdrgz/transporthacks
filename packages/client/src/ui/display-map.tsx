6495497f7a610b01173426b4aee201d4219b44ad
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-24 03:09:07 -0500

    fix infinite loop

--- packages/client/src/ui/display-map.tsx
+++ packages/client/src/ui/display-map.tsx
@@ -4,29 +4,46 @@ import { TLatLng } from '@/lib/utils';
 import {
   GoogleMap,
   useJsApiLoader,
-  Polyline,
   MarkerF,
   DirectionsRenderer,
-  DirectionsService,
 } from '@react-google-maps/api';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface IDisplayMapProps {
   startPoint: Exclude<TLatLng, null>;
   endPoint: Exclude<TLatLng, null>;
-  directions?: any;
 }
 
 export const DisplayMap: React.FC<IDisplayMapProps> = ({
   startPoint,
   endPoint,
-  directions,
 }) => {
   const [response, setResponse] = useState<any | null>(null);
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
   });
+
+  useEffect(() => {
+    if (isLoaded) {
+      const directionsService = new window.google.maps.DirectionsService();
+
+      directionsService.route(
+        {
+          origin: startPoint,
+          destination: endPoint,
+          travelMode: window.google.maps.TravelMode.DRIVING,
+        },
+        (result, status) => {
+          if (status === 'OK') {
+            setResponse(result);
+          } else {
+            console.log('ERROR FETCHING DIRECTIONS: ', status);
+          }
+        }
+      );
+    }
+  }, [isLoaded, startPoint, endPoint]);
 
   return isLoaded ? (
     <GoogleMap
@@ -39,23 +56,6 @@ export const DisplayMap: React.FC<IDisplayMapProps> = ({
       {response !== null && (
         <DirectionsRenderer options={{ directions: response }} />
       )}
-      <DirectionsService
-        options={{
-          origin: startPoint,
-          destination: endPoint,
-          // @ts-ignore
-          travelMode: 'DRIVING',
-        }}
-        callback={(result, status) => {
-          if (result !== null) {
-            if (status === 'OK') {
-              setResponse(result);
-            } else {
-              console.log('result: ', result);
-            }
-          }
-        }}
-      />
     </GoogleMap>
   ) : (
     <div>Loading....</div>
