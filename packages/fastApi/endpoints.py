4338274ac1c94ebd220c65c90ebf30dcb5637ca0
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2023-09-23 20:31:51 -0400

    Update endpoints.py

--- packages/fastApi/endpoints.py
+++ packages/fastApi/endpoints.py
@@ -3,7 +3,7 @@ import uvicorn
 from sklearn.linear_model import LogisticRegression
 from pydantic import BaseModel
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 
 app = FastAPI()
 
@@ -129,34 +129,53 @@ def predict_maintenance(data : request_body_maint):
 ###################################################
 #Matching 
 
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# import pandas as pd
-# import numpy as np
+## Load the drivers' data from the CSV file
+travel_df = pd.read_csv('Travel.csv')
+users_df = pd.read_csv('Users.csv')
 
+# Define the request data structure
+class UserRequestBody(BaseModel):
+    balance: float
+    reputation: float
+    preferred_latitude: float
+    preferred_longitude: float
 
-
-# # Load the drivers' data from the CSV file
-# travel_df = pd.read_csv('Travel.csv')
-
-# # Define the request data structure
-# class UserRequestBody(BaseModel):
-#     id: str
-#     balance: float
-#     reputation: float
-#     preferred_latitude: float
-#     preferred_longitude: float
-
-# # ... [Include the haversine_distance and find_best_match_for_user functions here] ...
-
-# # Endpoint to match a user with a driver
-# @app.post('/match_driver')
-# def match_driver(data: UserRequestBody):
-#     # Convert the request data to a pandas Series (to be compatible with our existing function)
-#     user_series = pd.Series(data.dict())
+# ... [Include the haversine_distance and find_best_match_for_user functions here] ...
+def find_best_match_for_user (user, df, users_df):
+    X_match = pd.DataFrame()
     
-#     # Use the matching function to get the matched driver's ID
-#     matched_driver_id = find_best_match_for_user(user_series, travel_df)
+    X_match['x_lat'] = df['startPlace.latitude']
+    X_match['x_long'] = df['startPlace.longitude']
+    X_match['y_lat'] = df['endPlace.latitude']
+    X_match['y_long'] =  df['endPlace.longitude']
     
-#     # Return the matched driver's ID
-#     return {'matched_driver_id': matched_driver_id}
+    y_balance = pd.DataFrame()
+    y_balance['balance'] = df['priceBalance']
+    
+    model_balance = LinearRegression()
+    model_balance.fit(X_match, y_balance)
+    
+    result_balance = model_balance.predict([user])
+    result_balance_1 = (result_balance[0][0])
+    result_balance_good = int(result_balance_1)
+    
+    users_df['balance'] = abs(users_df['balance'])
+    
+    for j in range(100):
+        for i in range(1000):
+            if users_df.iloc[i]['balance'] == result_balance_good+j:
+                return users_df.iloc[i]['id']
+    
+    
+     
+# Endpoint to match a user with a driver
+@app.post('/match_driver')
+def match_driver(data: UserRequestBody):
+    # Convert the request data to a pandas Series (to be compatible with our existing function)
+    user_series = pd.Series([data]) ############## Only not sure
+    
+    # Use the matching function to get the matched driver's ID
+    matched_driver_id = find_best_match_for_user(user_series, travel_df, users_df)
+    
+    # Return the matched driver's ID
+    return {'matched_driver_id': matched_driver_id}
