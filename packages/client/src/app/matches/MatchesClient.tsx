70b06a09b14066f25aaa45a886dca22a8edfdf33
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-24 03:07:18 -0500

    Fix dummy data

--- packages/client/src/app/matches/MatchesClient.tsx
+++ packages/client/src/app/matches/MatchesClient.tsx
@@ -1,25 +1,51 @@
 'use client';
 
-import { Match } from '@/features/matches/Match';
+import { useUserPreferences } from '@/context/user-preference-context';
+import { IMatch, Match } from '@/features/matches/Match';
+import { allCapsToFirstLetterCapitalized } from '@/lib/utils';
 
 const DUMMY_POTENTIAL_MATCHES = [
   {
     name: 'Jane Smith',
     email: 'janesmith@fakeemail.com',
-    startPoint: '123 Main St., Anywhere TX 12345',
-    endPoint: '456 Second St., Anywhere TX 12345',
+    startAddress: '5832 E Belknap St, Haltom City, TX 76117',
+    endAddress: '2009 E Abram St, Arlington, TX 76010',
+    startPoint: {
+      lat: 32.80893215812972,
+      lng: -97.25602490739479,
+    },
+    endPoint: {
+      lat: 32.7360523257754,
+      lng: -97.07539445211121,
+    },
   },
   {
     name: 'Joseph Pratt',
     email: 'josephpratt@fakeemail.com',
-    startPoint: '123 5th St., Anywhere TX 12345',
-    endPoint: '456 Garland Ave., Anywhere TX 12345',
+    startAddress: '5832 E Belknap St, Haltom City, TX 76117',
+    endAddress: '2009 E Abram St, Arlington, TX 76010',
+    startPoint: {
+      lat: 32.80893215812972,
+      lng: -97.25602490739479,
+    },
+    endPoint: {
+      lat: 32.7360523257754,
+      lng: -97.07539445211121,
+    },
   },
   {
     name: 'Mark Zuckerburg',
     email: 'markyboi@meta.com',
-    startPoint: '58423 9th St., Anywhere TX 12345',
-    endPoint: '456 Austin Blvd., Anywhere TX 12345',
+    startAddress: '5832 E Belknap St, Haltom City, TX 76117',
+    endAddress: '2009 E Abram St, Arlington, TX 76010',
+    startPoint: {
+      lat: 32.80893215812972,
+      lng: -97.25602490739479,
+    },
+    endPoint: {
+      lat: 32.7360523257754,
+      lng: -97.07539445211121,
+    },
   },
 ];
 
@@ -27,15 +53,22 @@ const DUMMY_OFFICIAL_MATCHES = [
   {
     name: 'Jeff Bezos',
     email: 'jeffyboi@amazon.com',
-    startPoint: '9825 8th St., Anywhere TX 12345',
-    endPoint: '4321 Transport Ave., Anywhere TX 12345',
+    startAddress: '5832 E Belknap St, Haltom City, TX 76117',
+    endAddress: '2009 E Abram St, Arlington, TX 76010',
+    startPoint: {
+      lat: 32.80893215812972,
+      lng: -97.25602490739479,
+    },
+    endPoint: {
+      lat: 32.7360523257754,
+      lng: -97.07539445211121,
+    },
   },
 ];
 
 export const MatchesClient = () => {
   return (
     <>
-      {/* Find a {BLANK = 'Driver' | 'Commuter'} */}
       <h1 className='text-5xl font-bold text-center'>Find a Partner</h1>
       <div className='flex flex-col gap-8'>
         <section>
@@ -43,22 +76,24 @@ export const MatchesClient = () => {
             Your Potential Matches
           </h2>
           <div className='flex flex-col gap-5'>
-            {DUMMY_POTENTIAL_MATCHES.map((match) => {
-              return (
-                <Match key={match.name} match={match} status='potential' />
-              );
-            })}
+            {DUMMY_POTENTIAL_MATCHES.map((match) => (
+              <Match key={match.name} match={match} status='potential' />
+            ))}
           </div>
         </section>
         <section>
           <h2 className='text-2xl font-semibold mb-3'>Your Official Matches</h2>
           <div className='flex flex-col gap-5'>
-            {DUMMY_OFFICIAL_MATCHES.map((match) => {
-              return <Match key={match.name} match={match} status='official' />;
-            })}
+            {DUMMY_OFFICIAL_MATCHES.map((match) =>
+              displayMatch(match, 'official')
+            )}
           </div>
         </section>
       </div>
     </>
   );
 };
+
+async function displayMatch(match: IMatch, status: 'potential' | 'official') {
+  return <Match key={match.name} match={match} status={status} />;
+}
